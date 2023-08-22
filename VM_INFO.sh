7dea6d38e52c5d2bfae8b606288bf5d68d7c2042
Seeramdas Pavan Raj <pavanraj97@gmail.com>
2023-08-21 22:34:39 -0300

    vm

---
+++ VM_INFO.sh
@@ -0,0 +1,27 @@
+#!bin/bash 
+
+######################## 
+# Author: Pavan 
+# Date: 21/08/2023
+#
+# This script outputs the node health
+#
+# Vesion: v1 
+######################## 
+
+set -x   # debug mode 
+
+
+echo "Disk space"
+
+df -h
+
+
+echo "Memory"
+
+free -g
+
+
+echo "CPU"
+
+nproc
